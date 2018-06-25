dbaea6bf2b24715bb3c69124728c61b95099f7a1
Noël Macé <contact@noelmace.com>
2018-06-25 14:33:55 +0200

    Fix fetch Peoples should return an empty array if it fails
    According to the existing UT, this is the expected behaviour.
    This is also a good partern, in order to lets the app continue to run as if no error occurred.

--- src/app/shared/people-service/people.service.ts
+++ src/app/shared/people-service/people.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
-import { HttpClient} from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse} from '@angular/common/http';
 import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs/Observable';
+import { catchError } from 'rxjs/operators';
+import { of } from 'rxjs/observable/of';
 
 @Injectable()
 export class PeopleService {
 
-    private _backendURL: any;
+    backendURL: any;
 
     constructor(private _http: HttpClient) {
-        this._backendURL = {};
+        this.backendURL = {};
 
         // build backend base url
         let baseUrl = `${environment.backend.protocol}://${environment.backend.host}`;
@@ -18,30 +20,41 @@ export class PeopleService {
         }
 
         // build all backend urls
-        Object.keys(environment.backend.endpoints).forEach(k => this._backendURL[k] = `${baseUrl}${environment.backend.endpoints[k]}`);
+        Object.keys(environment.backend.endpoints).forEach(k => this.backendURL[k] = `${baseUrl}${environment.backend.endpoints[k]}`);
+    }
+
+    private handleError(result = {}) {
+      return (error: HttpErrorResponse): Observable<any> => {
+        console.error(error);
+        // Let the app keep running by returning a safe result.
+        return of( result );
+      };
     }
 
     fetch(): Observable<any> {
-        return this._http.get(this._backendURL.allPeople);
+        return this._http.get(this.backendURL.allPeople)
+        .pipe(
+          catchError(this.handleError([]))
+        );
     }
 
     fetchRandom(): Observable<any> {
-        return this._http.get(this._backendURL.randomPeople);
+        return this._http.get(this.backendURL.randomPeople);
     }
 
     fetchOne(id: string): Observable<any> {
-        return this._http.get(this._backendURL.onePeople.replace(':id', id));
+        return this._http.get(this.backendURL.onePeople.replace(':id', id));
     }
 
     delete(id: string): Observable<any> {
-        return this._http.delete(this._backendURL.onePeople.replace(':id', id));
+        return this._http.delete(this.backendURL.onePeople.replace(':id', id));
     }
 
     update(person: any): Observable<any> {
-        return this._http.put(this._backendURL.onePeople.replace(':id', person.id), person);
+        return this._http.put(this.backendURL.onePeople.replace(':id', person.id), person);
     }
 
     create(person): Observable<any> {
-        return this._http.post(this._backendURL.allPeople, person);
+        return this._http.post(this.backendURL.allPeople, person);
     }
 }
